194015b6b60991b720b8a39f97bbc9b2aca779fa
PedroLima7011 <pedro.dlima@sptech.school>
2026-04-27 16:13:41 -0300

    Fix: Configurando a API web data viz

--- public/js/validacoes.js
+++ public/js/validacoes.js
@@ -46,6 +46,47 @@ function cadastrar() {
     if (criterios == 4) {
         div_mensagem.innerHTML = '<span class="realizado">Cadastro realizado com sucesso!'
     }
+
+    // Enviando o valor da nova input
+    fetch("/usuarios/cadastrar", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        // crie um atributo que recebe o valor recuperado aqui
+        // Agora vá para o arquivo routes/usuario.js
+        nomeServer: nome,
+        sobrenomeServer: sobrenome,
+        emailServer: email,
+        senhaServer: senha
+        }),
+    })
+      .then(function (resposta) {
+        console.log("resposta: ", resposta);
+    
+        if (resposta.ok) {
+          cardErro.style.display = "block";
+    
+          mensagem_erro.innerHTML =
+            "Cadastro realizado com sucesso! Redirecionando para tela de Login...";
+    
+          setTimeout(() => {
+            window.location = "login.html";
+          }, "2000");
+    
+          limparFormulario();
+          finalizarAguardar();
+        } else {
+          throw "Houve um erro ao tentar realizar o cadastro!";
+        }
+      })
+      .catch(function (resposta) {
+        console.log(`#ERRO: ${resposta}`);
+        finalizarAguardar();
+      });
+    
+    return false;
 }
 
 /* Login */
