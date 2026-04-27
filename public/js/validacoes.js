8fcf857bddcbf388b97ef42c33d3bcbfcb4732c2
PedroLima7011 <pedro.dlima@sptech.school>
2026-04-27 18:28:22 -0300

    Fix: Ajustando o login com web data viz

--- public/js/validacoes.js
+++ public/js/validacoes.js
@@ -17,87 +17,122 @@ function cadastrar() {
     }
 
     if (nome != '') {
-        criterios ++; /* 1° critério */
-        
+        criterios++; /* 1° critério */
+
         if (sobrenome != '') {
-            criterios ++; /* 2° critério */
-            
-            if (validoEmail ==  true) {
-                criterios ++; /* 3° critério */
-                
+            criterios++; /* 2° critério */
+
+            if (validoEmail == true) {
+                criterios++; /* 3° critério */
+
                 if (confirmarSenha == senha) {
-                    criterios ++; /* 4° critério */
+                    criterios++; /* 4° critério */
                 } else {
                     div_mensagem.innerHTML = `<span class="erro"> As senhas não coincidem`;
                 }
-            
+
             } else {
                 div_mensagem.innerHTML = `<span class="erro"> Insira um email válido`;
             }
-        
+
         } else {
             div_mensagem.innerHTML = `<span class="erro"> Insira seu sobrenome`;
         }
-    
+
     } else {
         div_mensagem.innerHTML = `<span class="erro"> Insira seu nome`;
     }
 
     if (criterios == 4) {
-        div_mensagem.innerHTML = '<span class="realizado">Cadastro realizado com sucesso!'
+        
+        // Enviando o valor da nova input
+        fetch("/usuarios/cadastrar", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                // crie um atributo que recebe o valor recuperado aqui
+                // Agora vá para o arquivo routes/usuario.js
+                nomeServer: nome,
+                sobrenomeServer: sobrenome,
+                emailServer: email,
+                senhaServer: senha
+            }),
+        })
+            .then(function (resposta) {
+                console.log("resposta: ", resposta);
+    
+                if (resposta.ok) {
+    
+                    div_mensagem.innerHTML =
+                        `<span class="realizado"> Cadastro realizado com sucesso!...`;
+    
+                    setTimeout(() => {
+                        window.location = "login.html";
+                    }, "2000");
+    
+                    limparFormulario();
+                } else {
+                    throw "Houve um erro ao tentar realizar o cadastro!";
+                }
+            })
+            .catch(function (resposta) {
+                console.log(`#ERRO: ${resposta}`);
+            });
+    
+        return false;
     }
 
-    // Enviando o valor da nova input
-    fetch("/usuarios/cadastrar", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        // crie um atributo que recebe o valor recuperado aqui
-        // Agora vá para o arquivo routes/usuario.js
-        nomeServer: nome,
-        sobrenomeServer: sobrenome,
-        emailServer: email,
-        senhaServer: senha
-        }),
-    })
-      .then(function (resposta) {
-        console.log("resposta: ", resposta);
-    
-        if (resposta.ok) {
-          cardErro.style.display = "block";
-    
-          div_mensagem.innerHTML =
-            "Cadastro realizado com sucesso! Redirecionando para tela de Login...";
-    
-          setTimeout(() => {
-            window.location = "login.html";
-          }, "2000");
-    
-          limparFormulario();
-        } else {
-          throw "Houve um erro ao tentar realizar o cadastro!";
-        }
-      })
-      .catch(function (resposta) {
-        console.log(`#ERRO: ${resposta}`);
-      });
-    
-    return false;
 }
 
 /* Login */
-let emailFicticio = `pedro@gmail.com`;
-let senhaFicticia = `Pedro123`;
-
 function logar() {
     let email = input_email.value;
     let senha = input_senha.value;
 
-    if (email == emailFicticio && senha == senhaFicticia) {
-        window.location.href = "dashboard.html"
-    } else {
-        div_mensagem.innerHTML = `<span class="erro"> Email ou senha inválidos`;
-    }
+    fetch("/usuarios/autenticar", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            emailServer: email,
+            senhaServer: senha
+        })
+    }).then(function (resposta) {
+        console.log("ESTOU NO THEN DO entrar()!")
+
+        if (resposta.ok) {
+            console.log(resposta);
+
+            resposta.json().then(json => {
+                console.log(json);
+                console.log(JSON.stringify(json));
+                sessionStorage.EMAIL_USUARIO = json.email;
+                sessionStorage.NOME_USUARIO = json.nome;
+                sessionStorage.SOBRENOME_USUARIO = json.sobrenome;
+                sessionStorage.ID_USUARIO = json.id;
+
+                setTimeout(function () {
+                    window.location = "quiz.html";
+                }, 1000); // apenas para exibir o loading
+
+            });
+
+        } else {
+
+            div_mensagem.innerHTML = `<span class="erro"> Email e/ou senha inválido(s)`;
+
+            resposta.text().then(texto => {
+                console.error(texto);
+            });
+        }
+
+    }).catch(function (erro) {
+        console.log(erro);
+    })
+
+    return false;
+
 }
--- src/controllers/usuarioController.js
+++ src/controllers/usuarioController.js
@@ -17,25 +17,19 @@ function autenticar(req, res) {
                     console.log(`Resultados: ${JSON.stringify(resultadoAutenticar)}`); // transforma JSON em String
 
                     if (resultadoAutenticar.length == 1) {
-                        console.log(resultadoAutenticar);
 
-                        aquarioModel.buscarAquariosPorEmpresa(resultadoAutenticar[0].empresaId)
-                            .then((resultadoAquarios) => {
-                                if (resultadoAquarios.length > 0) {
-                                    res.json({
-                                        id: resultadoAutenticar[0].id,
-                                        email: resultadoAutenticar[0].email,
-                                        nome: resultadoAutenticar[0].nome,
-                                        sobrenome: resultadoAutenticar[0].sobrenome,
-                                        senha: resultadoAutenticar[0].senha                                    });
-                                } else {
-                                    res.status(204).json({ aquarios: [] });
-                                }
-                            })
+                        res.json({
+                            id: resultadoAutenticar[0].id,
+                            email: resultadoAutenticar[0].email,
+                            nome: resultadoAutenticar[0].nome,
+                            sobrenome: resultadoAutenticar[0].sobrenome,
+                            senha: resultadoAutenticar[0].senha
+                        });
+
                     } else if (resultadoAutenticar.length == 0) {
-                        res.status(403).send("Email e/ou senha inválido(s)");
+                        div_mensagem.innerHTML = `<span class="erro"> Email e/ou senha inválido(s)`;
                     } else {
-                        res.status(403).send("Mais de um usuário com o mesmo login e senha!");
+                        div_mensagem.innerHTML = `<span class="erro"> Mais de um usuário com o mesmo login e senha!`;
                     }
                 }
             ).catch(
