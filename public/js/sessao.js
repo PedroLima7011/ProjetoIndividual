e8446c1594e42a3c08f259b05a2bdb082c54a059
PedroLima7011 <pedro.dlima@sptech.school>
2026-05-27 19:14:22 -0300

    Feat: Adiciona novas funcionalidades de sessão, com as funções limparSessao, validarSessao e sessao, para melhorar a experiência do usuário

--- public/js/sessao.js
+++ public/js/sessao.js
@@ -1,21 +1,22 @@
 // sessão
 function validarSessao() {
     var email = sessionStorage.EMAIL_USUARIO;
-    var nome = sessionStorage.NOME_USUARIO;
 
-    var b_usuario = document.getElementById("b_usuario");
-
-    if (email != null && nome != null) {
-        b_usuario.innerHTML = nome;
-    } else {
+    if (email == null) {
         window.location = "../login.html";
     }
 }
 
 function limparSessao() {
     sessionStorage.clear();
-    window.location = "../login.html";
+    window.location = "../index.html";
 }
 
+function sessao() {
+     var email = sessionStorage.EMAIL_USUARIO;
 
-
+    if (email != null) {
+        document.getElementById('logar').style.display = "none"
+        document.getElementById('cadastrar').style.display = "none"
+    }
+}
